e172c3f1883cfe178d3b29376c6934a8a8c8b3ac
wy315700 <wy315700@gmail.com>
2013-06-30 18:19:20 +0800

    add 178

--- data.py
+++ data.py
@@ -1,21 +1,20 @@
 import os
 import MySQLdb
 
-conn = MySQLdb.connect(host='192.168.1.100', user='root',passwd='asdfghjkl')
+conn = MySQLdb.connect(host='127.0.0.1', user='root',passwd='1234567890')
 conn.select_db('data')
 cursor = conn.cursor()
 
-dir1 = 'D:\\database\\7k7k\\2000-1\\'
-dir2 = 'D:\\database\\17173_6208\\'
-
+dir1 = '/home/wangyang/data/7k7k/2000-1/'
+dir2 = '/home/wangyang/data/17173_6208/'
+dir3 = '/home/wangyang/data/178/'
 def read_to_mysql(dir,explode,table,start,end):
     i =0;
     cursor.execute("""CREATE TABLE IF NOT EXISTS `%s`(  
   `id` INT(10) NOT NULL AUTO_INCREMENT,
   `account` VARCHAR(50),
   `password` VARCHAR(50),
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX (`account`)
+  PRIMARY KEY (`id`)
     );""",table)
     cursor.execute("truncate table `%s`",table)
     for filename in os.listdir(dir):
@@ -29,7 +28,7 @@ def read_to_mysql(dir,explode,table,start,end):
             list = list[start:end]
 
             if i % 10000 == 0 :
-                print list
+                print i
             i+=1
             params = []
             params.append(table)
@@ -38,7 +37,7 @@ def read_to_mysql(dir,explode,table,start,end):
                 cursor.execute("insert into `%s` (account,password) values (%s,%s)",params)
             except KeyboardInterrupt:
                 break
-            except Exception, e:
+            except :
                 pass
             else:
                 pass
@@ -78,7 +77,7 @@ def read_to_mysql_17173(dir,explode,table,start,end):
                 cursor.execute("insert into `%s` (account,password,email,clearpassword) values (%s,%s,%s,%s)",params)
             except KeyboardInterrupt:
                 break
-            except Exception, e:
+            except :
                 pass
             else:
                 pass
@@ -86,7 +85,10 @@ def read_to_mysql_17173(dir,explode,table,start,end):
                 pass
 
             line = file.readline()
-# read_to_mysql(dir1,'\t','7k7k',0,1)
+
+read_to_mysql(dir1,'\t','7k7k',0,2)
 
 read_to_mysql_17173(dir2,'\t|\t','17173',0,4)
+
+read_to_mysql(dir3,'\t','178',1,3)
 
