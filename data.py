7fc463f508d479ac39aae1af0bf6639a8d373a0d
wangyang <wy315700@gmail.com>
2013-09-07 20:50:27 +0800

    add yiban ecnu

--- data.py
+++ data.py
@@ -19,7 +19,8 @@ dir7 = datadir+'weibo/'
 dir8 = datadir+'renren/'
 dir9 = datadir+'aipai/'
 dir10 = datadir+'mop/'
-
+dir11 = 'ecnu/'
+dir12 = 'yiban/'
 
 mysql_create_7k7k = """CREATE TABLE IF NOT EXISTS `7k7k`(  
                     `id` INT(10) NOT NULL AUTO_INCREMENT,
@@ -75,17 +76,53 @@ mysql_create_renren = """CREATE TABLE IF NOT EXISTS `renren`(
                     `password` VARCHAR(50),
                     PRIMARY KEY (`id`)
                     );"""
-mysql_create_aipai = """CREATE TABLE IF NOT EXISTS `renren`(  
+mysql_create_aipai = """CREATE TABLE IF NOT EXISTS `aipai`(  
                     `id` INT(10) NOT NULL AUTO_INCREMENT,
                     `account` VARCHAR(50) unique,
                     `password` VARCHAR(50),
                     `email` VARCHAR(50),
                     PRIMARY KEY (`id`)
                     );"""
-mysql_create_mop = """CREATE TABLE IF NOT EXISTS `renren`(  
+mysql_create_mop = """CREATE TABLE IF NOT EXISTS `mop`(  
                     `id` INT(10) NOT NULL AUTO_INCREMENT,
                     `account` VARCHAR(50) unique,
                     `password` VARCHAR(50),
+                    PRIMARY KEY (`id`)
+                    );"""
+
+mysql_create_ecnu = """CREATE TABLE IF NOT EXISTS `ecnu`(  
+                    `id` INT(10) NOT NULL AUTO_INCREMENT,
+                    `account` VARCHAR(50) unique,
+                    `realname` VARCHAR(50),
+                    `identify` VARCHAR(50),
+                    `email` VARCHAR(50),
+                    `password` VARCHAR(50),
+                    PRIMARY KEY (`id`)
+                    );"""
+
+mysql_create_ecnu_student = """CREATE TABLE IF NOT EXISTS `ecnu_student`(  
+                    `id` INT(10) NOT NULL AUTO_INCREMENT,
+                    `sid` VARCHAR(50) unique,
+                    `realname` VARCHAR(50),
+                    `gender` VARCHAR(50),
+                    `identify` VARCHAR(50),
+                    `homephone` VARCHAR(50),
+                    `cellphone` VARCHAR(50),
+                    `otherphone` VARCHAR(50),
+                    `email` VARCHAR(50),
+                    `yuanxi` VARCHAR(50),
+                    `zhuanye` VARCHAR(50),
+                    `type` VARCHAR(50),
+                    `begintime` VARCHAR(50),
+                    `password` VARCHAR(50),
+                    PRIMARY KEY (`id`)
+                    );"""
+mysql_create_yiban = """CREATE TABLE IF NOT EXISTS `yiban`(  
+                    `id` INT(10) NOT NULL,
+                    `account` VARCHAR(50),
+                    `password` VARCHAR(50),
+                    `lasttime` VARCHAR(50),
+                    `lastip` VARCHAR(50),
                     PRIMARY KEY (`id`)
                     );"""
 
@@ -99,6 +136,7 @@ cursor.execute(mysql_create_weibo)
 cursor.execute(mysql_create_renren)
 cursor.execute(mysql_create_aipai)
 cursor.execute(mysql_create_mop)
+cursor.execute(mysql_create_yiban)
 mysql_insert_7k7k = "insert into `7k7k` (account,password) values (%s,%s)"
 mysql_insert_178 = "insert into `178` (account,password) values (%s,%s)"
 mysql_insert_17173 = "insert into `17173` (account,password_md5,email,password) values (%s,%s,%s,%s)"
@@ -109,6 +147,8 @@ mysql_insert_weibo = "insert into `weibo` (account,password) values (%s,%s)"
 mysql_insert_renren = "insert into `renren` (account,password) values (%s,%s)"
 mysql_insert_aipai = "insert into `aipai` (account,password,email) values (%s,%s)"
 mysql_insert_mop = "insert into `mop` (account,password) values (%s,%s)"
+mysql_insert_ecnu = "insert into `ecnu` (account,realname,identify,email,password) values (%s,%s,%s,%s,%s)"
+mysql_insert_yiban = "insert into `yiban` (id,account,password,lasttime,lastip) values (%s,%s,%s,%s,%s)"
 
 
 def read_to_mysql(dir,sql,table,start,end):
@@ -135,6 +175,10 @@ def read_to_mysql(dir,sql,table,start,end):
                 list = line.split()
             else:
                 list = line.split(explode)
+
+            if list[0] == 'null':
+                line = file.readline()
+                continue
             list = list[start:end]
             param = []
             for l in list:
@@ -176,7 +220,10 @@ def read_to_mysql(dir,sql,table,start,end):
 
 # read_to_mysql(dir8,mysql_insert_renren,'renren',0,3)
 
-read_to_mysql(dir9,mysql_insert_aipai,'aipai',0,3)
+# read_to_mysql(dir9,mysql_insert_aipai,'aipai',0,3)
 
-read_to_mysql(dir10,mysql_insert_mop,'mop',0,2)
- 
+# read_to_mysql(dir10,mysql_insert_mop,'mop',0,2)
+
+# read_to_mysql(dir11,mysql_insert_ecnu,'ecnu',0,5)
+
+read_to_mysql(dir12,mysql_insert_yiban,'yiban',0,5)
